defca41f2741449e8e6c2280ddbdeae2fc9f68b6
Misgan Moges <misganmoges03@gmail.com>
2022-08-06 17:18:04 +0300

    fiveth commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
 import { Component } from 'react';
 import Menu from './component/menuComponent'
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
 
   render(){
     return(
@@ -15,7 +24,7 @@ class App extends Component {
           </div>
         </Navbar>
 
-        <Menu />
+        <Menu dishes = {this.state.dishes} />
       </div>
     );
   }
